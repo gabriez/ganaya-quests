4146d8c20c0417c9f60e62077b3edb47b73693eb
gabotdev <gabrieltorres2014@gmail.com>
2026-06-01 13:31:24 -0400

    added form

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,8 +2,13 @@ import { Dice } from "@/icons/Dice";
 
 export const Login = () => {
 	return (
-		<div className="flex flex-col items-center justify-center gap-4">
+		<div className="flex flex-col items-center justify-center gap-4 w-4/5">
 			<Dice />
+			<form className="bg-[#FFFFFF]/20 w-[90%]">
+				<div className="border rounded-sm bg-[#171F33] text-[#87929A] border-[#3E484F]">
+					<input type="text" name="" id="" className="w-full" />
+				</div>
+			</form>
 		</div>
 	);
 };
